ff02d34a986a185b7cfe103403ddc5d698eed066
Kevin.Shindel <kevin.shindel@@yahoo.com>
2019-03-26 17:13:53 +0200

    data check

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from main.views import main
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
