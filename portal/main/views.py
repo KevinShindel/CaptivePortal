038bda71cca6aa358f0ee32eeb5b1b784c7cd61d
Kevin.Shindel <kevin.shindel@@yahoo.com>
2019-03-26 16:42:56 +0200

    kurlik

--- portal/main/views.py
+++ portal/main/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from git import Repo
+
+from portal.settings import PROJECT_ROOT
 from .models import Main
+
 
 # Create your views here.
 def main(request):
+    print(PROJECT_ROOT)
+    repo = Repo(PROJECT_ROOT)
+    branch = repo.active_branch.name
+    context = {'branch': branch}
     if request.method == 'POST':
         record = Main()
         record.login=request.POST.get('email', 'default')
         record.password = request.POST.get('pass', 'default')
         record.save()
 
-    # context = ''
-    return render(request, 'index.html', )
+    return render(request, 'index.html', context)
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -121,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+PROJECT_ROOT = os.path.join(BASE_DIR, '..')
