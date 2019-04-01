38e6b22c1dde3949ea002cce8d4f02cc9646ca72
Kevin.Shindel <kevin.shindel@@yahoo.com>
2019-04-01 16:22:32 +0300

    Mysql connect

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portal_db',
+        'USER': 'root',
+        'PASSWORD': 'fresstylers',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
