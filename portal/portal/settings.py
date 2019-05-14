cba53f2201369b331d0d219781c86e613c3ca2e0
Kevin.Shindel <kevin.shindel@@yahoo.com>
2019-05-14 15:07:33 +0300

    sqlite migration

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY 
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,19 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portal.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'portal_db',
         'USER': 'root',
-        'PASSWORD': 'fresstylers',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
     }
